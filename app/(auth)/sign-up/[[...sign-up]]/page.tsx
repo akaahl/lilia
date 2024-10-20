ecd5638d01be9395d0b56824eacd7ac9f0011a1b
Khair Afiq <a.k.afiq@live.com>
2024-10-20 21:35:46 +0800

    Style sign in  and sign up pages, configure env variables,etc

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { ClerkLoaded, ClerkLoading, SignUp } from "@clerk/nextjs";
+import Logo from "@/public/Logo.svg";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
 
-export default function SignUpPge() {
+export default function SignUpPage() {
   return (
-    <div>
-      <div>Sign up</div>
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2e2427]">Welcome back!</h1>
+          <p className="text-base text-[#7e8ca0]">
+            Log in or create a new account to get back to your dashboard.
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignUp path="/sign-up" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animated-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full bg-blue-100 hidden lg:flex items-center justify-center">
+        <Image
+          src={Logo}
+          height={300}
+          width={300}
+          alt="Logo"
+        />
+      </div>
     </div>
   );
 }
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  return <Button>Click me</Button>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <UserButton />
+    </div>
+  );
 }
