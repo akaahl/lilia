eba745ce79d6e4a01fbc83ff30b8703b97535e0b
Khair Afiq <a.k.afiq@live.com>
2024-10-20 20:59:53 +0800

    Setup basic auth and dashboard pages

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SignInPge() {
+  return (
+    <div>
+      <p>This is an authenticated route</p>
+    </div>
+  );
+}
---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SignUpPge() {
+  return (
+    <div>
+      <div>Sign up</div>
+    </div>
+  );
+}
