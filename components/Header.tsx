2604cf11bbc0616f0eac1cb336d70d41eb681853
Khair Afiq <a.k.afiq@live.com>
2024-10-21 19:09:52 +0800

    Create and add Navigation and NavButton components

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,14 @@
 import HeaderLogo from "./HeaderLogo";
+import Navigation from "./Navigation";
 
 export default function Header() {
   return (
-    <header className="bg-gradient-to-b from-rose-500 to-rose-300 px-4 py-8 lg:px-14 pb-36">
+    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36">
       <div className="max-w-screen-2xl mx-auto">
         <div className="w-full flex items-center justify-between mb-14">
           <div className="flex items-center lg:gap-x-16">
             <HeaderLogo />
+            <Navigation />
           </div>
         </div>
       </div>
---
+++ components/Navigation.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import NavButton from "./NavButton";
+
+const routes = [
+  {
+    href: "/",
+    label: "Overview",
+  },
+  {
+    href: "/transactions",
+    label: "Transactions",
+  },
+  {
+    href: "/accounts",
+    label: "Accounts",
+  },
+  {
+    href: "/categories",
+    label: "Categories",
+  },
+  {
+    href: "/settings",
+    label: "Settings",
+  },
+];
+
+export default function Navigation() {
+  const pathname = usePathname();
+
+  return (
+    <nav className="hidden lg:flex items-center gap-x-2 over-x-auto">
+      {routes.map((route) => (
+        <NavButton
+          key={route.href}
+          href={route.href}
+          label={route.label}
+          isActive={pathname === route.href}
+        />
+      ))}
+    </nav>
+  );
+}
