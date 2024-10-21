f63cbc2de82a9516035ea03393c907524f18d137
Khair Afiq <a.k.afiq@live.com>
2024-10-22 03:45:15 +0800

    Create a post route for account

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,19 +1,10 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import accounts from "./accounts";
-import { HTTPException } from "hono/http-exception";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
-
-app.onError((err, c) => {
-  if (err instanceof HTTPException) {
-    return err.getResponse();
-  }
-
-  return c.json({ err: "Internal error" }, 501);
-});
 
 const routes = app.route("/accounts", accounts);
 
