ed35274f3408b8c32ed966b493d194e092e467d5
Khair Afiq <a.k.afiq@live.com>
2024-10-23 03:43:47 +0800

    Add Skeleton component, and render it during fetching accounts data

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -3,15 +3,33 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/useNewAccount";
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 import { columns } from "./Columns";
 import { DataTable } from "@/components/DataTable";
 import { useGetAccounts } from "@/features/accounts/api/useGetAccounts";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function AccountsPage() {
   const { onOpen } = useNewAccount();
-  const { data } = useGetAccounts();
+  const { data, isLoading } = useGetAccounts();
   const accounts = data || [];
+
+  if (isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+            <Skeleton className="h-8 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
 
   return (
     <div className="max-w-screen-2xl mx-auto pb-10 -mt-24">
