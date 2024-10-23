892ed16752bb8212723a03dfb4d250f472764dae
Khair Afiq <a.k.afiq@live.com>
2024-10-24 02:57:09 +0800

    Add Categories page route and components

---
+++ app/(dashboard)/categories/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useNewAccount } from "@/features/accounts/hooks/useNewAccount";
+import { Loader2, Plus } from "lucide-react";
+import { columns } from "./Columns";
+import { DataTable } from "@/components/DataTable";
+import { useGetAccounts } from "@/features/accounts/api/useGetAccounts";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useBulkDeleteAccounts } from "@/features/accounts/api/useBulkDelete";
+
+export default function CategoriesPage() {
+  const { onOpen } = useNewAccount();
+  const accountsQuery = useGetAccounts();
+  const deleteAccounts = useBulkDeleteAccounts();
+  const accounts = accountsQuery.data || [];
+
+  const disabled = deleteAccounts.isPending || accountsQuery.isLoading;
+
+  if (accountsQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+            <Skeleton className="h-8 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <div className="max-w-screen-2xl mx-auto pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">
+            Categories page
+          </CardTitle>
+          <Button
+            size="sm"
+            onClick={onOpen}
+          >
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            columns={columns}
+            data={accounts}
+            filterKey="name"
+            onDelete={(row) => {
+              const ids = row.map((r) => r.original.id);
+              deleteAccounts.mutate({ ids });
+            }}
+            disabled={disabled}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
